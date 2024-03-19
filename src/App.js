87d8c6939bbde89fd82b789a27980e713dc60032
Dali-Hili <dalihili69@gmail.com>
2024-03-19 13:43:39 +0100

    photo gallery section

--- src/App.js
+++ src/App.js
@@ -1,11 +1,13 @@
 import React from "react";
 // import { Home } from "./pages/home/Home";
-import Footer from "./components/footer/Footer.jsx"
+// import Footer from "./components/footer/Footer.jsx"
+import PhotoGal from "./components/photoGal/PhotoGal.jsx"
 function App() {
   return (
     <div className="app">
       {/* <Home></Home> */}
-      <Footer></Footer>
+      <PhotoGal></PhotoGal>
+      {/* <Footer></Footer> */}
     </div>
   );
 }
